31e73b9c2e4d59d9703e2c432f071a05f01a3681
Michael Nelson <miken83@gmail.com>
2013-03-12 22:19:25 -0500

    Updated frontend for new authentications system.
    Added Simple_form
    Updated several forms to bootstrap and simple_form
    Added Sign in, Sign out, register and profile link to nave
    Added a collection select to our statu from.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jquery-rails'
 
 gem 'devise'
 
+gem 'simple_form'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_many :statuses
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -8,4 +11,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
+
+  def full_name
+  	first_name + " " + last_name
+  end
 end
