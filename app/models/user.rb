55edd98f69f9e3c21fbb653f562918f0cc854bcb
Michael Nelson <miken83@gmail.com>
2013-03-12 23:44:10 -0500

    Wrote test for sign up validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,16 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                           format: {
+                            with: /\A[a-zA-Z0-9_-]+\Z/,
+                            message: 'Must be formatted correctly'
+                          }
+
+
   def full_name
   	first_name + " " + last_name
   end
